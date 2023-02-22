f5fafe56fcf73b9328afe7d9a1693381b0738940
Tran My Linh <linhninh2@gmail.com>
2023-02-22 18:57:33 +0700

    init

---
+++ DemoWebLayoutDB/Controllers/HomeController.cs
@@ -0,0 +1,45 @@
+﻿using DemoWebLayoutDB.Models;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
+using System.Diagnostics;
+using X.PagedList;
+
+namespace DemoWebLayoutDB.Controllers
+{
+    public class HomeController : Controller
+    {
+        QlbanVaLiContext db = new QlbanVaLiContext();
+        private readonly ILogger<HomeController> _logger;
+
+        public HomeController(ILogger<HomeController> logger)
+        {
+            _logger = logger;
+        }
+
+        public IActionResult Index(int page = 1)
+        {
+            int pageNumber = page;
+            int pageSize = 8;
+            var lstanpham = db.TDanhMucSps.AsNoTracking().OrderBy(x => x.TenSp);
+            PagedList<TDanhMucSp> pagelist = new PagedList<TDanhMucSp>(lstanpham, pageNumber, pageSize);
+            return View(pagelist);
+        }
+
+        public IActionResult PLSanPham(string MaLoai)
+        {
+            var lstanpham = db.TDanhMucSps.Where(x=>x.MaLoai==MaLoai).OrderBy(x=>x.TenSp).ToList();
+            return View(lstanpham);
+        }
+
+        public IActionResult Privacy()
+        {
+            return View();
+        }
+
+        [ResponseCache(Duration = 0, Location = ResponseCacheLocation.None, NoStore = true)]
+        public IActionResult Error()
+        {
+            return View(new ErrorViewModel { RequestId = Activity.Current?.Id ?? HttpContext.TraceIdentifier });
+        }
+    }
+}
